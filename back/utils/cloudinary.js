03b9b2e135c29594a07338ee000441bd5d44ee78
Baimini <98502673+Baimini@users.noreply.github.com>
2024-11-22 15:49:39 +0800

    back

---
+++ back/utils/cloudinary.js
@@ -0,0 +1,8 @@
+const cloudinary = reauire("cloudinary").v2;
+cloudinary.config({
+    cloud_name: "dhahvfevb",
+    api_key: "",
+    api_secret: "UHv4gdnemLHGe73GBdfizmAUBzY",
+});
+
+module.exports = cloudinary;
